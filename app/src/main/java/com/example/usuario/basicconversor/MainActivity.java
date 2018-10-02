baf81c5eeaf48b02fef3387116aaa4a913c81474
Damián Cabello <jdamiancabello@gmail.com>
2018-10-02 12:50:03 +0200

    Aplicación completamente funcional.

--- app/src/main/java/com/example/usuario/basicconversor/MainActivity.java
+++ app/src/main/java/com/example/usuario/basicconversor/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.usuario.basicconversor;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -10,23 +12,50 @@ import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
-    @BindView(R.id.button) Button btnConvertir;
-    @BindView(R.id.tvEuros) TextView tvEuros;
-    @BindView(R.id.tvDolares) TextView tvDolares;
-    @BindView(R.id.rbt_euros) RadioButton rtb_euros;
-    @BindView(R.id.rbt_dolares) RadioButton rbt_dolares;
+    private final double CAMBIO = 0.85;
+
+    private EditText tvDolar;
+    private EditText tvEuro;
+    private Button btnConvert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+
+
+        tvEuro = findViewById(R.id.tvEuros);
+        tvDolar = findViewById(R.id.tvDolares);
+        btnConvert = findViewById(R.id.button);
     }
 
 
-    private String Conversor(RadioGroup grupo){
-
+    private String DolarEuro(final double CAMBIO, double cantidad) {
+        return String.format("%.2f", cantidad * CAMBIO);
     }
+
+    private String EuroDolar(final double CAMBIO, double cantidad) {
+        return String.format("%.2f", cantidad / CAMBIO);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (((RadioButton) findViewById(R.id.rbt_euros)).isChecked()) {
+            tvDolar.setText(EuroDolar(CAMBIO, Conversion(tvEuro.getText().toString())));
+        } else if (((RadioButton) findViewById(R.id.rbt_dolares)).isChecked()) {
+            tvEuro.setText(DolarEuro(CAMBIO, Conversion(tvDolar.getText().toString())));
+        }
+    }
+    private double Conversion(String valor)
+    {
+        try {
+            return Double.parseDouble(valor);
+        }
+        catch (Exception e){
+            return 0;
+        }
+    }
+
 }
